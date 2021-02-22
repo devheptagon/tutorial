b26f77224b7f46cb59a1d801488246354cf85c53
elifserver <elifknk@gmail.com>
2021-02-22 19:26:29 +0000

    frogJumps and divisibleCount

--- index.js
+++ index.js
@@ -14,6 +14,36 @@ export function reverse(str) {
     let current = characters[i];
     result += current;
   }
-console.log("elif");
   return result;
 }
+
+
+export function frogJumps(from, to, jumpCapacity){
+  if(from <= 0 || to <= 0 ||jumpCapacity <= 0 || 
+     from > 1000000000 || to > 1000000000 || jumpCapacity > 1000000000 || 
+     to <= from) {
+      return -1; 
+  }
+
+  let jumpCount = Math.floor((to - from)/jumpCapacity); 
+  let modeForJumps = (to - from) % jumpCapacity;
+  modeForJumps > 0 ? jumpCount++ : jumpCount;
+  return jumpCount;
+  
+  }
+
+  export function divisibleCount(startRange, endRange, divisibleBy){
+      if (startRange < 0 || endRange < 0 || divisibleBy <= 0
+          || startRange > 2000000000 || endRange > 2000000000 || divisibleBy > 2000000000
+          || startRange >= endRange){
+              return -1;
+      }
+
+      let divisibleByCount = 0;
+      console.log(startRange);
+      console.log(endRange);
+      for(let i = startRange; i <= endRange; i++){
+          i % divisibleBy == 0 ? divisibleByCount++ : divisibleByCount;
+      }
+      return divisibleByCount;
+    }
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-import { sumAll, reverse } from "./index.js";
+import { sumAll, reverse, frogJumps, divisibleCount } from "./index.js";
 
 describe("index function are being tested", function () {
   test("sumAll is being tested", function () {
@@ -8,4 +8,26 @@ describe("index function are being tested", function () {
   test("reverse is tested", function () {
     expect(reverse("hello ebru")).toBe("urbe olleh");
   });
+
+  test("frog jump count" , function () {
+    expect(frogJumps(10,90,20)).toBe(4);
+    expect(frogJumps(10,5,20)).toBe(-1);
+    expect(frogJumps(10,1000000001,20)).toBe(-1);
+    expect(frogJumps(10,10,20)).toBe(-1);
+    expect(frogJumps(10,100000000,1000000001)).toBe(-1);
+    expect(frogJumps(10,10,-9)).toBe(-1);
+
+  });
+
+  test("divisible by count in a range of number" , function () {
+    expect(divisibleCount(10,15,3)).toBe(2);
+    expect(divisibleCount(10,10,3)).toBe(-1);
+    expect(divisibleCount(10,15,0)).toBe(-1);
+    expect(divisibleCount(15,10,5)).toBe(-1);
+    expect(divisibleCount(10,15,-3)).toBe(-1);
+    expect(divisibleCount(10,15,0)).toBe(-1);
+    expect(divisibleCount(2000000002,2000000045,3)).toBe(-1);
+    expect(divisibleCount(10,2000000002,3)).toBe(-1);
+  });
+
 });
